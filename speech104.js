60fbeb1cff74073ea2d14045dc4260f65eeec3bf
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2022-11-26 18:37:30 -0300

    Add files via upload
    Click the mouse to  start talking to the program

---
+++ speech104.js
@@ -0,0 +1,29 @@
+// speech104.js:  
+// 10.4 Speech Recognition with p5.speech
+//
+  let lang;
+  let speechRec;
+  let continuous = true;
+  function setup() {
+  noCanvas();
+  lang = 'en-US';
+
+} //setup
+  
+function mouseClicked() {
+  speechRec = new p5.SpeechRec(lang, gotSpeech);
+  speechRec.start(continuous);
+}
+  function gotSpeech() {
+     console.log(speechRec);
+     objeto = speechRec.resultString;
+     confidence = speechRec.resultConfidence;
+     console.log(confidence);
+     console.log(objeto);
+     if(confidence < 0.6){
+        console.log ('?');
+     }
+  }
+ 
+
+
