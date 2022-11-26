270705ab3b7af1a65dd068d3c3a9c28fb5d1355c
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2022-11-26 19:42:02 -0300

    Add files via upload

--- speech104.js
+++ speech104.js
@@ -4,15 +4,17 @@
   let lang;
   let speechRec;
   let continuous = true;
+  let voice;
+  
   function setup() {
   noCanvas();
   lang = 'en-US';
-
 } //setup
   
 function mouseClicked() {
   speechRec = new p5.SpeechRec(lang, gotSpeech);
   speechRec.start(continuous);
+  voice = new p5.Speech();
 }
   function gotSpeech() {
      console.log(speechRec);
@@ -20,8 +22,10 @@ function mouseClicked() {
      confidence = speechRec.resultConfidence;
      console.log(confidence);
      console.log(objeto);
+     voice.speak(objeto);
+     objeto = null;
      if(confidence < 0.6){
-        console.log ('?');
+     console.log ('?');
      }
   }
  
